b925db71910fddaa34aa88a2f97b116628f8b3d9
Vincent <vincent.cote@uleth.ca>
2020-03-21 12:10:32 -0600

    tfidf seems to work

--- tf_idf.py
+++ tf_idf.py
@@ -1,17 +1,11 @@
 import math
 from nltk import word_tokenize
 from nltk import FreqDist
-
-def get_tf(t,d):
-    count = 0
-    for word in word_tokenize(d):
-        if word == t:
-            count = count + 1
-    return 1 + math.log10(count) if count > 0 else 0
-
+from nltk.corpus import brown
 
 def get_word_matrix(words):
     list = []
+    # print(words)
     words = word_tokenize(words)
     for w in FreqDist(words):
         list.append(w)
@@ -19,33 +13,41 @@ def get_word_matrix(words):
 
 
 def get_idf_matrix(wrd_matrix, documents, n):
+    print('getting idf...')
     n += 0.0
     list = []
     doc_dist = []
     for d in documents:
-        doc_dist.append(FreqDist(word_tokenize(d)))
+        # doc_dist.append(FreqDist(word_tokenize(d)))
+        doc_dist.append(FreqDist(d))
     for word in wrd_matrix:
         count = 0
         for d in doc_dist:
             if d[word] > 0:
                 count = count + 1
-        list.append(math.log10(n/count))
+        if count > 0:
+            list.append(math.log10(n/count))
 
     return list
 
 
 def get_tf_matrix(wrd_matrix, documents):
+    print('getting tf...')
     list = []
     for d in documents:
         doc_list = []
+        doc_freq = FreqDist(d)
         for w in wrd_matrix:
-            tf = get_tf(w,d)
+            count = doc_freq[w]
+            tf = 1 + math.log10(count) if count > 0 else 0
+            # tf = get_tf(w,d)
             doc_list.append(tf)
         list.append(doc_list)
     return list
 
 
 def get_w_matrix(idf_matrix, tf_matrix):
+    print('getting w...')
     list = []
     for d in tf_matrix:
         w_list = []
@@ -59,7 +61,7 @@ def get_w_matrix(idf_matrix, tf_matrix):
 
 
 def cosine(d1,d2):
-    print(len(d1))
+    print('calculating cosine...')
     sum = 0.0
     i = 0
     d1d = 0.0
@@ -70,30 +72,46 @@ def cosine(d1,d2):
         d1d += d1[i] * d1[i]
         d2d += d2[i] * d2[i]
         i += 1
-    return (sum/(math.sqrt(d1d) * math.sqrt(d2d)))
+    return (sum/(math.sqrt(d1d) * math.sqrt(d2d))) if d1d > 0 or d2d > 0 else 0
 
-# Bring in actual corpus
-docs = ['food restaurant customer restaurant waitress', 'food store customer cashier', 'appliance store customer store cashier']
+# To calculate similarities between documents of the same category uncomment one of the
+# following categories:
+
+# cat = 'news'
+# cat = 'editorial'
+# cat = 'reviews'
+# cat = 'religion'
+# cat = 'hobbies'
+# cat = 'lore'
+# cat = 'belle_lettres'
+cat = 'government'
+# cat = 'learned'
+# cat = 'fiction'
+# cat = 'mystery'
+# cat = 'science_fiction'
+# cat = 'adventrue'
+# cat = 'romance'
+# cat = 'humor'
+
+fid = brown.fileids(categories=cat)
+docs = []
+for f in fid:
+    docs.append(brown.words(fileids=[f]))
 N = len(docs)
 all_words = ''
-
 for doc in docs:
-    all_words = all_words + ' ' + doc
-
+    for word in doc:
+        all_words = all_words + ' ' + word
 
 word_matrix = get_word_matrix(all_words)
 idf_matrix = get_idf_matrix(word_matrix, docs, N)
 tf_matrix = get_tf_matrix(word_matrix, docs)
 w_matrix = get_w_matrix(idf_matrix, tf_matrix)
 
+# To compare different documents within the cluster change the index
+# value for doc1 and doc2
 
-
-
-
-# print(word_matrix)
-# print(idf_matrix)
-# print(tf_matrix)
-# print(get_w_matrix(idf_matrix, tf_matrix))
-print(cosine(w_matrix[1], w_matrix[2]))
-
+doc1 = 0
+doc2 = 1
+print(cosine(w_matrix[doc1], w_matrix[doc2]))
 
